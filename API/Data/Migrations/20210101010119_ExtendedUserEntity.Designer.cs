bd6618da7047d1c62663223c8ecc591b6b51afae
Derek <derek.caswell@gmail.com>
2020-12-31 23:04:29 -0700

    End of section 9

--- API/Data/Migrations/20210101010119_ExtendedUserEntity.Designer.cs
+++ API/Data/Migrations/20210101010119_ExtendedUserEntity.Designer.cs
@@ -45,7 +45,7 @@ namespace API.Data.Migrations
                     b.Property<string>("Introduction")
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("KownAs")
+                    b.Property<string>("KnownAs")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("LastActive")
